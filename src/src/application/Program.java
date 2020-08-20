4f672c051a14734d05444c3bcc02c33bf626a6db
jvelajr <jose.vela.jr@outlook.com>
2020-08-20 17:49:12 -0300

    Inclusao da classe Position

--- src/src/application/Program.java
+++ src/src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
+import bordgame.Position;
+
 public class Program {
 
     public static void main(String[] args) {
 
         System.out.println("Hello world!!!");
+
+        Position pos = new Position(12,18);
+        System.out.println(pos);
 
     }
 }
